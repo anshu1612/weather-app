102f2264ea641c02e5eabbdbaa807e5be227c5d4
anshu1612 <anshuchakrawarti26@gmail.com>
2025-01-18 19:09:23 +0530

    added onSubmit

--- index.js
+++ index.js
@@ -15,23 +15,12 @@ function fetchDataFromForm(event) {
     fetchData(city);
   }
 
-searchBtn.addEventListener("click", () => {
-  let city = document.getElementById("city").value;
-  if (city == "") {
-    window.alert("Please Enter City");
-  }
-  if (city != "") {
-    fetchData(city);
-  }
-});
-
 async function fetchData(city) {
   const apikey = "195a4d1e9cd34df6bfe83016251801";
   const response = await fetch(
     `https://api.weatherapi.com/v1/current.json?key=${apikey}&q=${city}&aqi=no`
   );
   const data = await response.json();
-  console.log(data);
   displayData(data);
 }
 
